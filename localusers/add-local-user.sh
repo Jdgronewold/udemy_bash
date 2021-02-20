ca791a74a42ce8eaaf567b03e0a0131025030f52
Jdgronewold <jeff.gronewold@gmail.com>
2021-02-20 10:07:56 -0700

    small changes to add local users

--- localusers/add-local-user.sh
+++ localusers/add-local-user.sh
@@ -15,7 +15,7 @@ read -p 'Enter password: ' PASSWORD
 # Create user
 useradd -c "${PERSON_NAME}" -m "${USER_NAME}"
 
-if [[ ${?} -ne 0 ]]
+if [[ "${?}" -ne 0 ]]
 then
   echo "User creation failed"
   exit 1
@@ -24,7 +24,7 @@ fi
 
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
-if [[ ${?} -ne 0 ]]
+if [[ "${?}" -ne 0 ]]
 then
   echo "password creation failed"
   exit 1
@@ -34,7 +34,11 @@ echo "Password creation success"
 # Force the user to create a new password on login
 passwd -e ${USER_NAME}
 
+echo
+
 echo "username: ${USER_NAME}"
 echo "name: ${PERSON_NAME}"
 echo "password: ${PASSWORD}"
-echo "hostname: ${hostname}"
+echo "hostname: ${HOSTNAME}"
+
+exit 0
